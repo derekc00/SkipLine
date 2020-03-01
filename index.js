0b94638ba15b18ceefb3805186c7e13093f54ecb
Ethan Song <ersong@uci.edu>
2020-02-29 20:39:59 -0800

    Revert "eject"
    This reverts commit 1f60b69b0614d9161b7365bf7f7468c3be409c85.

--- index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry, Platform } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('skipLine', () => App);
-
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('skipLine', { rootTag });
-}
