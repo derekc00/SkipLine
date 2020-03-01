5a5de10f1caa4b7323b3b34948f54da107f1c4c9
Ethan Song <ersong@uci.edu>
2020-02-29 22:44:12 -0800

    Update firebaseFunctions.js

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -3,25 +3,27 @@ import { Request } from './consumerRequests';
 import * as firebase from 'firebase';
 import '@firebase/firestore';
 
-export function sendRequest(request, addComplete){
+export function sendRequest(request, addComplete) {
     console.log("a");
 
-    const firebaseConfig = { 
+    const firebaseConfig = {
         apiKey: "AIzaSyAkia1lQi1pkrLZS0YTBXuZW5LMCGGPjyI",
         authDomain: "skipline-5cd96.firebaseapp.com",
         databaseURL: "https://skipline-5cd96.firebaseio.com",
         projectId: "skipline-5cd96",
     }
 
-    if (!firebase.apps.length) {firebase.initializeApp(firebaseConfig)};
+    if (!firebase.apps.length) { firebase.initializeApp(firebaseConfig) };
 
     const dbh = firebase.firestore();
 
-    dbh.collection("characters").add({
-        employment: "plumber",
-        outfitColor: "red",
-        specialAttack: "fireball"
-    })
+    dbh.collection("requests").add({
+        userID: request.name,
+        partySize: request.partySize,
+        price: request.price,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    }).then((data) => addComplete(data))
+        .catch((error) => console.log(error))
 
     // firebase.firestore()
     // .collection("request")
